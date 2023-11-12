bf200950271615afdd5bd9cfe957309d68af1b8b
Thomas Rialan <thomasrialan@gmail.com>
2023-11-12 18:13:13 +0100

    file inspection; gp

--- tpu_graphs/baselines/layout/data.py
+++ tpu_graphs/baselines/layout/data.py
@@ -741,6 +741,7 @@ def get_npz_split(
     glob_pattern = os.path.join(split_path, "*.npz")
     files = tf.io.gfile.glob(glob_pattern)[:3]
 
+    import pdb;pdb.set_trace() 
     #print("ONLY USING SOME FILES FOR EXP!!!!")
     if not files:
         raise ValueError("No files matched: " + glob_pattern)
