65c16fbe3a2004d53cf92eb158f7eeedc67ca031
Thomas Rialan <thomasrialan@gmail.com>
2023-11-13 17:04:26 +0100

    baseline

--- tpu_graphs/baselines/layout/data.py
+++ tpu_graphs/baselines/layout/data.py
@@ -730,7 +730,7 @@ def get_npz_split(
     """Returns data for a single partition."""
     glob_pattern = os.path.join(split_path, "*.npz")
     #files = sorted(tf.io.gfile.glob(glob_pattern))
-    files = tf.io.gfile.glob(glob_pattern)[:3]
+    files = tf.io.gfile.glob(glob_pattern)
 
     #print("ONLY USING SOME FILES FOR EXP!!!!")
     if not files:
